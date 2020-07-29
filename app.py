0c80a1a97c97d8f5a0b7541e262dfbfa23353652
Mugumya Twarik Harouna <51790725+twarik@users.noreply.github.com>
2020-07-29 22:16:58 +0300

    Update app.py

--- app.py
+++ app.py
@@ -9,17 +9,44 @@ from bokeh.transform import factor_cmap
 import warnings
 warnings.filterwarnings('ignore')
 
+#Define tools to add on the plots
+TOOLS = 'pan,zoom_out,box_zoom,reset,save'
+
 st.sidebar.markdown('''# Sales Analysis''')
 
-#Read in dataset
 @st.cache(allow_output_mutation=True)
 def load_data():
+    '''
+    Function to read-in the dataset
+    '''
     return pd.read_csv('./sales_data.csv')
+
+def create_figure(x_label,y_label,height,TOOLS,x_type=None,x_ticks=None,title=None):
+    '''
+    Function to Create a new figure for plotting.
+    '''
+    if x_type==None:
+        fig = figure(tools = TOOLS, title=title,
+                    x_axis_label=x_label, y_axis_label=y_label,
+                    height=height, x_range=x_ticks
+                    )
+    else:
+        fig = figure(tools = TOOLS, title=title,
+                    x_axis_label=x_label, y_axis_label=y_label,
+                    height=height, x_axis_type=x_type
+                    )
+    return fig
+
+def hover(tips):
+    '''
+    Function to create a hoover inspector tool
+    '''
+    tool = HoverTool()
+    tool.tooltips=tips
+    return tool
 
 data = load_data()
 
-#Define tools to add on the plots
-TOOLS = 'pan,zoom_out,box_zoom,reset,save'
 #----------------------DATA CLEANING-------------------------------------------
 data['Order Date'] = pd.to_datetime(data['Order Date'])
 #----------------------FEATURE ENGINEERING-------------------------------------
@@ -50,35 +77,27 @@ if status == 'What were the best and worst months for sales?':
             'August', 'September', 'October', 'November', 'December']
     grouped_months.reset_index(inplace=True)
     grouped_months.month = months
+    source = ColumnDataSource(grouped_months)
 
     st.markdown('''
     ## What was the best/worst month for sales?
     ### How much was earned that month?
     ''')
-    source = ColumnDataSource(grouped_months)
 
-    p = figure(tools = TOOLS,
-        x_axis_label='Months',
-        y_axis_label='Sales in USD ($)',
-        height=400,
-        x_range=months
-        )
+    p = create_figure(TOOLS=TOOLS,x_label='Months',y_label='Sales in USD ($)',height=400,x_ticks=months)
 
     p.vbar(x='month', top='Sales', width=0.9, source=source,
        fill_color=factor_cmap('month', palette=Paired12[::-1], factors=grouped_months['month']))
 
-    hover = HoverTool()
-    hover.tooltips=[
-        ('Month', "@month"),
-        ('Sales', "$@Sales"),
-    ]
-    p.add_tools(hover)
+    h = hover([('Month', "@month"), ('Sales', "$@Sales")])
+    p.add_tools(h)
     st.bokeh_chart(p, use_container_width=True)
 
 #-------2. Which cities had the highest and lowest sales?--------------------
 elif status == 'Which cities had the highest and lowest sales?':
     group_city = data.groupby('City').sum()
     group_city.reset_index(inplace=True)
+    source = ColumnDataSource(group_city)
 
     st.markdown(
     '''
@@ -86,26 +105,13 @@ elif status == 'Which cities had the highest and lowest sales?':
     ### How much was earned from every city?
     '''
     )
-    source = ColumnDataSource(group_city)
 
-    p = figure(tools = TOOLS,
-        x_axis_label='Cities',
-        y_axis_label='Sales in USD ($)',
-        height=400,
-        x_range=group_city['City']
-        )
+    p = create_figure(TOOLS=TOOLS,x_label='Cities',y_label='Sales in USD ($)',height=400,x_ticks=group_city['City'])
+    p.vbar(x='City', top='Sales', width=0.9, source=source, fill_color=factor_cmap('City', palette=Paired12, factors=group_city['City']))
 
-    p.vbar(x='City', top='Sales', width=0.9, source=source,
-       fill_color=factor_cmap('City', palette=Paired12, factors=group_city['City']))
-
-    hover = HoverTool()
-    hover.tooltips=[
-        ('City', "@City"),
-        ('Sales', "$@Sales"),
-    ]
-    p.add_tools(hover)
+    h = hover([('City', "@City"),('Sales', "$@Sales")])
+    p.add_tools(h)
     p.xaxis.major_label_orientation = "vertical"
-
     st.bokeh_chart(p, use_container_width=True)
 
 #-------------------3. What is the peak purchasing time?-----------------------
@@ -116,27 +122,15 @@ elif status == 'What is the peak purchasing time?':
 
     '## What is the peak purchasing time?'
 
-    p = figure(tools = TOOLS,
-        title='Daily purchase profile',
-        x_axis_label='Hours',
-        y_axis_label='No. of transactions',
-        height=400,
-        )
-
+    p = create_figure(TOOLS=TOOLS,title='Daily purchase profile',x_label='Hours',y_label='No. of transactions',height=400,x_type='linear')
     p.circle(x='hour', y='Sales', radius=0.15, fill_color='green', source=source)
     p.line(x='hour', y='Sales', line_width=2.5, line_color='red', source=source)
 
-    hover = HoverTool()
-    hover.tooltips=[
-        ('Transactions', "@Sales"),
-        ('Hour', "@hour"),
-    ]
-    p.add_tools(hover)
-
+    h = hover([('Transactions', "@Sales"), ('Hour', "@hour")])
+    p.add_tools(h)
     st.bokeh_chart(p, use_container_width=True)
 
     t = group_hour.Sales.idxmax()
-
     'The peak purchasing time is %sh' %t
 
 #--------------4. What is the demand for each product?--------------------------
@@ -145,28 +139,18 @@ elif status == 'What is the demand for each product?':
     group_product.reset_index(inplace=True)
     group_product['radius'] = group_product['Sales']/group_product['Sales'].max()
     products = group_product['Product'].tolist()
-
     source = ColumnDataSource(group_product)
 
     '## What is the demand for each product?'
 
-    p = figure(tools = TOOLS,
-        x_axis_label='Product',
-        y_axis_label='Products sold annually',
-        height=500,
-        x_range=products,
-        )
+    p = create_figure(TOOLS=TOOLS,x_label='Product',y_label='Products sold annually',height=500,x_ticks=products)
 
     p.asterisk(x='Product', y='Sales', fill_color='black', source=source)
     p.circle(x='Product', y='Sales', radius='radius', line_color='grey', source=source,
             fill_color=factor_cmap('Product', palette=Category20_20[:len(products)], factors=products), alpha=0.7)
 
-    hover = HoverTool()
-    hover.tooltips=[
-        ('Product', "@Product"),
-        ('No. of products sold', "@Sales"),
-    ]
-    p.add_tools(hover)
+    h = hover([('Product name', "@Product"), ('# Units sold', "@Sales")])
+    p.add_tools(h)
     p.xaxis.major_label_orientation = "vertical"
     st.bokeh_chart(p, use_container_width=True)
 
@@ -176,38 +160,24 @@ elif status == 'Sales trend analysis':
     group_product.reset_index(inplace=True)
     products = group_product['Product'].tolist()
 
-    source = ColumnDataSource(group_product)
-
     '## Sales trend analysis by product'
+    multi_prod = st.multiselect('Type/select product/s for trend analysis', (products))
 
-    multi_prod = st.multiselect('Type/select product(s) for trend analysis', (products))
-
-    p = figure(tools = TOOLS,
-        x_axis_label='Date',
-        y_axis_label='Sales',
-        height=300,
-        x_axis_type='datetime')
+    p = create_figure(TOOLS=TOOLS,x_label='Date',y_label='Daily sales',height=300,x_type='datetime')
 
     for idx, val in enumerate(multi_prod):
         product_df = data.loc[data['Product'] == val]
         product_df=product_df.groupby('Date').sum()
         product_df.reset_index(inplace=True)
-
         source = ColumnDataSource(product_df)
-
         p.line(x='Date', y='Sales', legend_label=val, line_color=Category20_20[idx], source=source)
 
-    hover = HoverTool()
-    hover.tooltips=[
-        ('', "$@Sales"),
-    ]
-    p.add_tools(hover)
-
+    h = hover([('', "$@Sales")])
+    p.add_tools(h)
     p.legend.location = "top_right"
     p.legend.orientation = "vertical"
     if len(multi_prod) !=0:
         p.add_layout(p.legend[0], 'right')
-
     st.bokeh_chart(p, use_container_width=True)
 
 else:
@@ -215,23 +185,16 @@ else:
 
     daily=data.groupby('Date').sum()
     daily.reset_index(inplace=True)
-
-    '#### Time series of the sales made on a daily basis during 2019'
     source = ColumnDataSource(daily)
 
-    p = figure(tools = TOOLS,
-        x_axis_label='Date',
-        y_axis_label='Sales',
-        height=300,
-        x_axis_type='datetime')
+    '#### Time series of the sales made on a daily basis during 2019'
+
+    p = create_figure(TOOLS=TOOLS,x_label='Date',y_label='Sales',height=300,x_type='datetime')
 
     p.line(x='Date', y='Sales', source=source)
 
-    hover = HoverTool()
-    hover.tooltips=[
-        ('Sales', "$@Sales"),
-    ]
-    p.add_tools(hover)
+    h = hover([('Sales', "$@Sales")])
+    p.add_tools(h)
     p.y_range.start = 350
     st.bokeh_chart(p, use_container_width=True)
 
